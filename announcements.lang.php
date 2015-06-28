cfc0f6e127d2d0e7f4c7c8d2a77624228da6e374
Qafar <qafar313@gmail.com>
2015-06-28 16:47:48 +0500

    Create announcements.lang.php

---
+++ announcements.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB 1.8 Azerbaijani Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_announcements'] = "Forum Elanları";
+$l['announcements'] = "Elanlar";
+$l['forum_announcement'] = "Forum Elanları: {1}";
+$l['error_invalidannouncement'] = "Elanda xəta var.";
+
+$l['announcement_edit'] = "Elanı redaktə et";
+$l['announcement_qdelete'] = "Elanı sil";
+$l['announcement_quickdelete_confirm'] = "Elanı silmək istədiyinizdən əminsinizmi?";
